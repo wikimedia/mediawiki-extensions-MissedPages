e917c761d58170f614fde6a2e602148ff2f17499
Jayprakash12345 <0freerunning@gmail.com>
2021-12-04 16:55:57 +0530

    Use new Hook system
    Change-Id: If89fc4036f25fee2dab73ca77f9f903b145a06a6

--- includes/Hooks.php
+++ includes/Hooks.php
@@ -4,18 +4,20 @@ namespace MediaWiki\Extension\MissedPages;
 
 use Article;
 use DatabaseUpdater;
+use MediaWiki\Installer\Hook\LoadExtensionSchemaUpdatesHook;
+use MediaWiki\Page\Hook\ShowMissingArticleHook;
 
 /**
  * Hook handlers for the MissedPages extension.
  */
-class Hooks {
+class Hooks implements LoadExtensionSchemaUpdatesHook, ShowMissingArticleHook {
 
 	/**
 	 * Set up MissedPages's database table.
 	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/LoadExtensionSchemaUpdates
 	 * @param DatabaseUpdater $updater
 	 */
-	public static function onLoadExtensionSchemaUpdates( DatabaseUpdater $updater ) {
+	public function onLoadExtensionSchemaUpdates( $updater ) {
 		$sqlDir = dirname( __DIR__ ) . '/sql';
 		$updater->addExtensionTable( MissedPages::TABLE_NAME, "$sqlDir/create_missed_pages_table.sql" );
 	}
@@ -25,7 +27,7 @@ class Hooks {
 	 * @link https://www.mediawiki.org/wiki/Manual:Hooks/ShowMissingArticle
 	 * @param Article $article
 	 */
-	public static function onShowMissingArticle( Article $article ) {
+	public function onShowMissingArticle( $article ) {
 		$log = new MissedPages();
 		$log->recordMissingPage( $article );
 	}
